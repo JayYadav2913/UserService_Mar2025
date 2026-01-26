09f46f01cc27dfea7409c5e7b81a3e30b5932883
Jay Yadav <work7028@gmail.com>
2026-01-26 17:14:40 +0530

    JWT Impl

--- src/main/java/com/scaler/userservice_mar2025/configs/ApplicationConfig.java
+++ src/main/java/com/scaler/userservice_mar2025/configs/ApplicationConfig.java
@@ -1,10 +1,14 @@
 package com.scaler.userservice_mar2025.configs;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
 
 @Configuration
 public class ApplicationConfig {
@@ -24,4 +28,11 @@ public class ApplicationConfig {
         );
         return httpsecurity.build();
     }
+
+        @Bean
+        public SecretKey getSecretKey() {
+            MacAlgorithm macAlgorithm = Jwts.SIG.HS256;
+            SecretKey secretKey= macAlgorithm.key().build();
+            return secretKey;
+        }
 }
--- src/main/java/com/scaler/userservice_mar2025/services/UserServiceImpl.java
+++ src/main/java/com/scaler/userservice_mar2025/services/UserServiceImpl.java
@@ -6,15 +6,17 @@ import com.scaler.userservice_mar2025.models.Token;
 import com.scaler.userservice_mar2025.models.User;
 import com.scaler.userservice_mar2025.repositories.TokenRepository;
 import com.scaler.userservice_mar2025.repositories.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -22,13 +24,16 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     private TokenRepository tokenRepository;
+    private SecretKey secretKey;
 
     public UserServiceImpl(UserRepository userRepository,
                            BCryptPasswordEncoder bCryptPasswordEncoder,
-                           TokenRepository tokenRepository) {
+                           TokenRepository tokenRepository,
+                           SecretKey secretKey) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.tokenRepository = tokenRepository;
+        this.secretKey = secretKey;
 
     }
 
@@ -75,45 +80,78 @@ public class UserServiceImpl implements UserService {
 //        //
 //        token.setTokenValue(RandomStringUtils.randomAlphanumeric(128));
 //
-//        Calendar calendar=Calendar.getInstance();
-//        calendar.add(Calendar.DAY_OF_YEAR,30);
-//        Date expiryDate = calendar.getTime();
-//        token.setExpiryAt(expiryDate);
 //
 //        return tokenRepository.save(token);
 
         //Generate a JWT Token using JJWT library.
-        
-        String payload = "{\n" +
-                " \"email\":\"anil@gmail.com\",\n" +
-                " \"userId\":\"2\",\n" +
-                " \"roles\":[\"STUDENT\"],\n" +
-                " \"expiry\":\"2026-07-30T12:34:56Z\",\n" +
-                "}";
 
-        byte[] payloadBytes = payload.getBytes(StandardCharsets.UTF_8);
+        //Hardcoded
+//        String payload = "{\n" +
+//                " \"email\":\"anil@gmail.com\",\n" +
+//                " \"userId\":\"2\",\n" +
+//                " \"roles\":[\"STUDENT\"],\n" +
+//                " \"expiry\":\"2026-07-30T12:34:56Z\",\n" +
+//                "}";
 
-        String token = Jwts.builder().content(payloadBytes).compact();
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("iss","scaler.com");
+        claims.put("userId",user.getId());
 
-        return token;
+        Calendar calendar=Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR,30);
+        Date expiryDate = calendar.getTime();
+
+        claims.put("exp",expiryDate);
+        claims.put("roles",user.getRoles());
+
+//        byte[] payloadBytes = payload.getBytes(StandardCharsets.UTF_8);
+//        String token = Jwts.builder().content(payloadBytes).compact();
+
+        String jwtToken=Jwts.builder().claims(claims).signWith(secretKey).compact();
+        return jwtToken;
 
     }
 
     @Override
     public User validateToken(String tokenValue) throws InvalidTokenException {
 
-        Optional<Token> optionalToken = tokenRepository.findByTokenValueAndExpiryAtGreaterThan(
-                tokenValue,
-                new Date());
+//        Optional<Token> optionalToken = tokenRepository.findByTokenValueAndExpiryAtGreaterThan(
+//                tokenValue,
+//                new Date());
+//
+//        if(optionalToken.isEmpty()){
+//            //Invalid Token
+//            throw new InvalidTokenException("Invalid token.");
+//        }
+//
+//        //Token is valid
+//        Token token = optionalToken.get();
+//        return token.getUser();
 
-        if(optionalToken.isEmpty()){
-            //Invalid Token
-            throw new InvalidTokenException("Invalid token.");
+        try {
+            JwtParser jwtParser = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build();
+
+            Claims claims = jwtParser
+                    .parseSignedClaims(tokenValue)
+                    .getPayload();
+
+            Object userIdObj = claims.get("userId");
+
+            Long userId;
+            if (userIdObj instanceof Integer) {
+                userId = ((Integer) userIdObj).longValue();
+            } else {
+                userId = (Long) userIdObj;
+            }
+
+            return userRepository.findById(userId)
+                    .orElseThrow(() -> new InvalidTokenException("User not found"));
+
+        } catch (Exception e) {
+            throw new InvalidTokenException("Invalid or expired JWT token");
         }
-
-        //Token is valid
-        Token token = optionalToken.get();
-        return token.getUser();
     }
 
     @Override
